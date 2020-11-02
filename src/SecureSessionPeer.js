7d37ca786ff6b088c0e06c05d098181c65d86d8f
Soner <73308906+sonergzn@users.noreply.github.com>
2020-11-02 11:33:38 +0100

    Updated_SecureSession

--- src/SecureSessionPeer.js
+++ src/SecureSessionPeer.js
@@ -1,84 +1,71 @@
-const libs = require('libsodium-wrappers');
-const Decryptor = require('../src/Decryptor');
-const Encryptor = require('../src/Encryptor');
-client = null;
-server = null;
+const nacl = require('libsodium-wrappers');
+const Decryptor = require('./Decryptor');
+const Encryptor = require('./Encryptor');
 
 
-module.exports = async () => {
 
-    await libs.ready;
-    var havePeers = null;
+module.exports = async (peer) => {
+    await nacl.ready;
+    const keypr = nacl.crypto_kx_keypair();
+ 
+    let p2;
+    let msg;
+    let tx, rx;
+    let encryptor, decryptor
+    let obj = Object.freeze({
+       publicKey: keypr.publicKey,
 
-    if (server == null) 
-    {
-        havePeers = false;
-        var keys = libs.crypto_kx_keypair();
 
-        server = 
-        {
-          publicKey: keys.publicKey, privateKey: keys.privateKey
-        
+       encrypt: (msg) => 
+       {
+            return encryptor.encrypt(msg);
+       },
+
+       decrypt: (ciphertext, nonce) => 
+       {
+            return decryptor.decrypt(ciphertext, nonce);
+       },
+
+       tMsg: (_msg) => 
+       {
+        msg = _msg;
+       },
+
+       send: (msg) => 
+       {
+        p2.tMsg(obj.encrypt(msg));
+       },
+
+       receive: () => 
+       {
+            return obj.decrypt(msg.ciphertext, msg.nonce);
+       },
+
+
+       generateSharedKeys: async (peer) => 
+       {
+        p2 = peer;      
+        const secKey = nacl.crypto_kx_server_session_keys(keypr.publicKey, keypr.privateKey, p2.publicKey);
+        rx = secKey.sharedRx;
+        tx = secKey.sharedTx;
+        decryptor = await Decryptor(rx);
+        encryptor = await Encryptor(tx);
         }
 
+    });
+
+
+    if (peer) 
+    {
+        p2 = peer;
+        const ckeys = nacl.crypto_kx_client_session_keys(keypr.publicKey, keypr.privateKey, p2.publicKey);
+        rx = ckeys.sharedRx;
+        decryptor = await Decryptor(rx);
+        tx = ckeys.sharedTx;
+        encryptor = await Encryptor(tx);
+        p2.generateSharedKeys(obj);
     }
 
 
-    else {
-        havePeers = true;
-
-        var keys = libs.crypto_kx_keypair();
-        const keysForClient = libs.crypto_kx_client_session_keys(keys.publicKey, keys.privateKey, server.publicKey);
-        const keysForServer = libs.crypto_kx_server_session_keys(server.publicKey, server.privateKey, keys.publicKey);
-
-
-        client = 
-        {
-          privateKey: keys.privateKey, publicKey: keys.publicKey,decryptor: await Decryptor(keysForClient.sharedRx),
-          encryptor: await Encryptor(keysForClient.sharedTx)
-        };
-
-        server.decryptor = await Decryptor(keysForServer.sharedRx);
-        server.encryptor = await Encryptor(keysForServer.sharedTx);
-    } 
-
-
-
-    return Object.freeze({
-        publicKey: havePeers ? client.publicKey : server.publicKey,
-        send: (msg) => 
-        {
-
-          havePeers ? server.message = client.encryptor.encrypt(msg) : client.message = server.encryptor.encrypt(msg);
-        },
-
-
-        receive: () => 
-        {
-            if (havePeers) 
-            {
-                msg = client.message;
-                return client.decryptor.decrypt(msg.ciphertext, msg.nonce);
-            } 
-            else 
-            {
-                msg = server.message;
-                return server.decryptor.decrypt(msg.ciphertext, msg.nonce);
-            }
-        },
-
-
-        encrypt: (msg) => 
-        {
-            var answer = null
-            havePeers ? answer = client.encryptor.encrypt(msg) : answer = server.encryptor.encrypt(msg);
-
-            return answer;
-        },
-
-
-        decrypt:(ciphertext, nonce) =>
-            {
-              return client.decryptor.decrypt(ciphertext, nonce)
-            }
-    }); };
+    return obj;
+}
